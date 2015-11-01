daf3b9d3b829dc2afbede0c7e9636af1c0e83922
Philippe Rey <filipre@PRMBP.local>
2015-11-01 10:31:34 -0500

    Dl

--- wp-content/themes/twentytwelve-child/page-download.php
+++ wp-content/themes/twentytwelve-child/page-download.php
@@ -12,7 +12,7 @@ get_header();
 	<div id="primary" class="site-content download-content">
 			
 		<form id="ac-download-form" name="ac-download-form" action="??????????" method="GET">
-			<div class="text">Enter your download code here:</div>
+			<div class="text">Enter your download code here yeah:</div>
 			<input class="ac-download-code" id="ac-download-code" type="text" name="code" placeholder="Code" />
 			<a id="ac-download-submit" class="submit" href="javascript:void(0)" onclick="dlFormSubmit()">Go</a>
 		</form>
