3f39e4848c1b90930e81778c0ce6b763610003f9
Philippe Rey <filipre@PRMBP.local>
2015-11-01 11:06:51 -0500

    Dl

--- wp-content/themes/twentytwelve-child/page-download.php
+++ wp-content/themes/twentytwelve-child/page-download.php
@@ -12,7 +12,10 @@ get_header();
 	<div id="primary" class="site-content download-content">
 			
 		<form id="ac-download-form" name="ac-download-form" action="??????????" method="GET">
-			<div class="text">Enter your download code here yeah:</div>
+			<div class="text">
+				<p>You're at the right place to download you record. Please insert -below- the code written on your download card and click on "<b>Go</b>". Easy, right ?</p> 
+				<p>If you have any problem, feel free to contact us at <a href="mailto:atelierciseaux@gmail.com">atelierciseaux@gmail.com</a></p>
+			</div>
 			<input class="ac-download-code" id="ac-download-code" type="text" name="code" placeholder="Code" />
 			<a id="ac-download-submit" class="submit" href="javascript:void(0)" onclick="dlFormSubmit()">Go</a>
 		</form>
