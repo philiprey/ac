263abc37dca89a7f8b1e9817d1e4a1f36dc202e6
Philippe Rey <filipre@PRMBP.local>
2015-11-01 21:51:28 -0500

    Dl form

---
+++ wp-content/themes/twentytwelve-child/download/download.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Download release file
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve_Child
+ */
+ 
+include_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
+global $wpdb;
+	
+if ( isset( $_GET['lease'] ) ) {
+	
+	/* get code & release details */
+	$release = $wpdb->get_row(
+		$wpdb->prepare(
+		    "SELECT r.*, c.ID as code, c.code_prefix, c.code_suffix FROM " . dc_tbl_releases() .
+		    " r INNER JOIN " . dc_tbl_codes() ." c ON c.release = r.ID WHERE MD5(CONCAT('wp-dl-hash',c.ID)) = %s",
+		    array( $_GET['lease'] )
+		)
+	);
+			    
+	/* get numbwe of downloads for current code */
+	$downloads = $wpdb->get_row(
+		$wpdb->prepare(
+		    "SELECT COUNT(*) AS downloads FROM " . dc_tbl_downloads() . " WHERE code= %s",
+		    array( $release->code )
+		)
+	);
+		
+		// Start download if maximum of allowed downloads is not reached
+		if ( $downloads->downloads < $release->allowed_downloads ) {
+			// Get current IP
+			$IP = $_SERVER['REMOTE_ADDR'];
+			
+			// Insert download in downloads table
+			$wpdb->insert(	dc_tbl_downloads(),
+							array( 'code' => $release->code, 'IP' => $IP),
+							array( '%d', '%s') );
+			
+			// If Apache's xsendfile is enabled (must be installed and working on server side)
+			if ( dc_xsendfile_enabled() ) {
+				header( 'X-Sendfile: ' . dc_file_location() . $release->filename );
+				header( 'Content-Type: application/octet-stream' );
+				header( 'Content-Disposition: attachment; filename=\"' . urlencode ( $release->filename ) . '\"' );
+				exit;
+			}
+			
+			// Increase timeout for slow connections
+			set_time_limit( 0 );
+			
+			// Deactivate output compression (required for IE, otherwise Content-Disposition is ignored)
+			if( ini_get( 'zlib.output_compression' ) ) {
+				ini_set( 'zlib.output_compression', 'Off' );
+			}
+			
+			// Content description
+			header( 'Content-Description: File Transfer' );
+			
+			// Content disposition
+			if ( strpos ( $_SERVER [ 'HTTP_USER_AGENT' ], "MSIE" ) > 0 )
+			{
+				header( 'Content-Disposition: attachment; filename="' . urlencode ( $release->filename ) . '"' );
+			}
+			else
+			{
+				header( 'Content-Disposition: attachment; filename*=UTF-8\'\'' . urlencode ( $release->filename ) );
+			}
+			
+			// Content type
+			$content_type = dc_header_content_type();
+			if ( $content_type == DC_HEADER_CONTENT_TYPE ) {
+				// Send MIME type of current file
+				header( 'Content-Type: ' . get_mime_content_type( dc_file_location() . $release->filename ) );
+			}
+			else {
+				// Override content type with header setting
+				header( 'Content-Type: ' . $content_type );
+			}
+			
+			// Transfer encoding
+			header( 'Content-Transfer-Encoding: binary' );
+			
+			// Content length
+			header( 'Content-Length: '.filesize( dc_file_location() . $release->filename ));
+			
+			// Cache handling
+			header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+			header( 'Pragma: public' );
+			header( 'Expires: 0' );
+			
+			// Stream file
+			ob_clean();
+			flush();
+			$handle = fopen( dc_file_location() . $release->filename, 'rb' );
+			$chunksize = 1 * ( 1024 * 1024 ); 
+			$buffer = '';
+			if ($handle === false) {
+				exit;
+			}
+			while (!feof($handle)) {
+				$buffer = fread($handle, $chunksize);
+				echo $buffer;
+				flush();
+			}
+
+			// Close file
+			fclose($handle);
+			
+			// Exit
+			exit;
+		}
+	}
+
+?>
