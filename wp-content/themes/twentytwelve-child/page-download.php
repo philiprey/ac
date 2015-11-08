1df5d0550580833a4b8c5e99d10dc48d6875eb04
Philippe Rey <philiprey@gmail.com>
2015-11-08 11:06:58 -0500

    Fix

--- wp-content/themes/twentytwelve-child/page-download.php
+++ wp-content/themes/twentytwelve-child/page-download.php
@@ -16,7 +16,7 @@ $url = get_stylesheet_directory_uri() . "/download/check-download-code.php";
 			<div class="text">
 				<p>You're at the right place to download your record.</p>
 				<p>Please insert -below- the code written on your download card and click on "<b>Go</b>". Easy, right ?</p> 
-				<p>If you have any problem, feel free to contact us at <a href="mailto:atelierciseaux@gmail.com">atelierciseaux@gmail.com</a></p>
+				<p>If you have any problem, feel free to contact us at <a href="mailto:atelierciseaux@gmail.com">atelierciseaux[at]gmail.com</a></p>
 			</div>
 			<input class="ac-download-code" id="ac-download-code" type="text" name="code" placeholder="Your code" />
 			<a id="ac-download-check-submit" class="submit" href="javascript:void(0)" onclick="dlCheckFormSubmit()"><span>Go</span></a>
